9b8bf19ef40283fb1d1a0e985e97fa0d4e42937b
Nicolle Romero <nicollearomero@gmail.com>
2020-07-29 17:07:00 -0700

    Completed coffee store

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Adds a row (selected item, string) to the cart
 const addItemToCart = (itemName) => {
   $('#cart-items').append(`
     <tr>
@@ -8,11 +9,14 @@ const addItemToCart = (itemName) => {
   `);
 };
 
+// Clears everything out of the cart and resets total to 0
 const resetCart = () => {
   $('#cart-total').html('0.00');
   $('#cart-items').empty();
 };
 
+
+// Calculates the total for all items in cart
 const incrementCartTotal = (price) => {
   const cartTotal = $('#cart-total');
 
@@ -22,6 +26,7 @@ const incrementCartTotal = (price) => {
   cartTotal.html(total.toFixed(2));
 };
 
+// Updates running total of coffees sold up top
 const incrementCoffeeSold = (amountSold) => {
   let coffeeSold = Number($('#coffee-sold-counter').html());
   coffeeSold += amountSold;
@@ -38,3 +43,22 @@ const setProgressAndStatus = (progressVal, statusMsg) => {
 //
 // Add your event handlers below.
 //
+
+const button = $('.add-to-order');
+
+button.on('click', () => {
+  addItemToCart('Coffee');
+  incrementCartTotal(1.50);
+  incrementCoffeeSold($('#cart-items').children().length);
+  setProgressAndStatus(50, "Successfully added to cart");
+});
+
+$('#place-order').on('click', () => {
+  setProgressAndStatus(100, "Your order is complete!");
+  resetCart();
+});
+
+$('#new-order').on('click', () => {
+  // $('#coffee-sold-counter').html('0')
+  setProgressAndStatus(0, "Start a new order...");
+});
